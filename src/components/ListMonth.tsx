13974e37f3cfcc065bbaf45e99e8e81d392b9f7a
illispi <44943503+illispi@users.noreply.github.com>
2024-10-02 10:56:22 +0300

    dayAdjust wip

--- src/components/ListMonth.tsx
+++ src/components/ListMonth.tsx
@@ -1,14 +1,70 @@
 import { For, type Component } from "solid-js";
 
-const daysInMonth = (year: number, month: number) => {
+const yearChange = (year: number, month: number, forward: boolean) => {
+	if (month === 11 && forward) {
+		return { month: 0, year: year + 1 };
+	}
+	if (month === 0 && !forward) {
+		return { month: 11, year: year - 1 };
+	}
+	if (forward) {
+		return { month: month + 1, year };
+	}
+	return { month: month - 1, year };
+};
+
+const dayAdjust = (month: number, year: number) => {
+	const adjustYearPos = yearChange(year, month, true);
+	const adjustYearNeg = yearChange(year, month, false);
+
+	const firstDay = new Date(year, month, 1).getDay();
+	const lastDayCurrent = new Date(new Date(year, month + 1, 1) - 1).getDay();
+	const lastDayPreviousMonth = new Date(new Date(year, month, 1) - 1).getDay();
+
+	const daysBehind = firstDay === 0 ? 6 : firstDay - 1;
+	const daysForward = firstDay === 0 ? 0 : 7 - firstDay;
+
+	const daysArr = [];
+	for (let i = 0; i < daysBehind; i++) {
+		daysArr.push(
+			new Date(
+				adjustYearNeg.year,
+				adjustYearNeg.month,
+				lastDayPreviousMonth - i,
+			).getDate(),
+		);
+	}
+	for (let i = 0; i < lastDayCurrent; i++) {
+		daysArr.push(new Date(year, month, i).getDate());
+	}
+	for (let i = 0; i < daysForward; i++) {
+		daysArr.push(
+			new Date(adjustYearPos.year, adjustYearPos.month, i).getDate(),
+		);
+	}
+
+	return daysArr;
+};
+
+const daysInGrid = (year: number, month: number) => {
 	const days = new Date(year, month, 0).getDate();
+	const weekdays = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+
 	return Array.from({ length: days }, (_, i) => i + 1);
 };
 
 const ListMonth: Component<{ month: number; year: number }> = (props) => {
 	return (
-		<div>
-			<For each={daysInMonth(props.year, props.month)}>
+		<div class="grid grid-cols-7 gap-4 max-w-5xl">
+			<For each={dayAdjust(props.year, props.month)}>
 				{(days, index) => <div>{days}</div>}
 			</For>
 		</div>
