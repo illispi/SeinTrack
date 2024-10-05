6eb5ade0aee57c089ce6e2f9a86fe5843fe176f6
illispi <44943503+illispi@users.noreply.github.com>
2024-10-05 08:13:58 +0300

    best i can do

--- src/server/hono.ts
+++ src/server/hono.ts
@@ -10,21 +10,22 @@ app.use(async (c, next) => {
 	await next();
 	if (c.res.status !== 200) {
 		// const obj = { result: { data: { json: "hello" } } };
-		const obj = {
-			error: {
-				json: {
-					message: "NOT_FOUND",
-					code: -32004,
-					data: {
-						code: "NOT_FOUND",
-						httpStatus: 404,
-						stack:
-							"TRPCError: NOT_FOUND\n    at eval (/home/sam/repos/SeinTrack/src/server/trpc/routers/mainRouter.ts:11:11)\n    at resolveMiddleware (file:///home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/index.mjs:421:36)\n    at callRecursive (file:///home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/index.mjs:451:38)\n    at resolve (file:///home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/index.mjs:481:30)\n    at Object.callProcedure (/home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/config-194bdd43.js:157:12)\n    at inputToProcedureCall (/home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/resolveHTTPResponse-b7a8a1c9.js:48:35)\n    at /home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/resolveHTTPResponse-b7a8a1c9.js:171:51\n    at Array.map (<anonymous>)\n    at Object.resolveHTTPResponse (/home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/resolveHTTPResponse-b7a8a1c9.js:171:32)",
-						path: "test",
-					},
-				},
-			},
-		};
+		const obj = { result: { data: { json: "hello" } } };
+		// const obj = {
+		// 	error: {
+		// 		json: {
+		// 			message: "NOT_FOUND",
+		// 			code: -32004,
+		// 			data: {
+		// 				code: "NOT_FOUND",
+		// 				httpStatus: 404,
+		// 				stack:
+		// 					"TRPCError: NOT_FOUND\n    at eval (/home/sam/repos/SeinTrack/src/server/trpc/routers/mainRouter.ts:11:11)\n    at resolveMiddleware (file:///home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/index.mjs:421:36)\n    at callRecursive (file:///home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/index.mjs:451:38)\n    at resolve (file:///home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/index.mjs:481:30)\n    at Object.callProcedure (/home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/config-194bdd43.js:157:12)\n    at inputToProcedureCall (/home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/resolveHTTPResponse-b7a8a1c9.js:48:35)\n    at /home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/resolveHTTPResponse-b7a8a1c9.js:171:51\n    at Array.map (<anonymous>)\n    at Object.resolveHTTPResponse (/home/sam/repos/SeinTrack/node_modules/@trpc/server/dist/resolveHTTPResponse-b7a8a1c9.js:171:32)",
+		// 				path: "test",
+		// 			},
+		// 		},
+		// 	},
+		// };
 
 		const blob = new Blob([JSON.stringify(obj, null, 2)], {
 			type: "application/json",
