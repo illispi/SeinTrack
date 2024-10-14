5d4499d73b2f3f166e609ec9d309012d206d56ae
illispi <44943503+illispi@users.noreply.github.com>
2024-10-14 12:14:05 +0300

    sidepanel

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,7 +1,11 @@
 import type { Component } from "solid-js";
 
 const SidePanel: Component = (props) => {
-	return <div class="fixed top-0 left-0 w-96">HELLO</div>;
+	return (
+		<div class="fixed top-0 left-0 w-96 border-r border-black min-h-screen h-full">
+			HELLO
+		</div>
+	);
 };
 
 export default SidePanel;
