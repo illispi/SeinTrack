c1628d40b60899ea7996ed4839b8a5fceadbd44c
illispi <44943503+illispi@users.noreply.github.com>
2024-10-14 10:54:39 +0300

    migrations

--- src/migrations/2024_10_03_11_17_41_base.ts
+++ src/migrations/2024_10_03_11_17_41_base.ts
@@ -9,16 +9,13 @@ export async function up(db: Kysely<any>): Promise<void> {
 		.addColumn("target_hours", "float4", (col) => col.defaultTo(3).notNull());
 	await db.schema
 		.createTable("dates")
-		//projects ref
 		.addColumn("id", "serial", (col) => col.primaryKey())
+		.addColumn("project_id", "text", (col) =>
+			col.references("projects.id").onDelete("cascade").notNull(),
+		)
 		.addColumn("hours_worked", "float4")
 		.addColumn("date", "timestamp", (col) => col.defaultTo(null))
 		.execute();
-	await db.schema
-		.createTable("todos")
-		.addColumn("id", "serial", (col) => col.primaryKey())
-		// tag ref must be included
-		.addColumn("todo", "text", (col) => col.notNull());
 
 	await db.schema
 		.createTable("tag_groups")
@@ -28,12 +25,29 @@ export async function up(db: Kysely<any>): Promise<void> {
 	await db.schema
 		.createTable("tags")
 		.addColumn("id", "serial", (col) => col.primaryKey())
-		//tag groups ref
+		.addColumn("tag_group_id", "text", (col) =>
+			col.references("tag_groups.id").onDelete("cascade"),
+		)
 		.addColumn("tag", "text");
+
+	await db.schema
+		.createTable("todos")
+		.addColumn("id", "serial", (col) => col.primaryKey())
+		.addColumn("tag_id", "text", (col) =>
+			col.references("tags.id"),
+		)
+		.addColumn("project_id", "text", (col) =>
+			col.references("projects.id").onDelete("cascade").notNull(),
+		)
+		.addColumn("date_id", "text", (col) => col.references("dates.id"))
+		.addColumn("todo", "text", (col) => col.notNull())
+		.addColumn("hours_worked", "float4");
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
 	await db.schema.dropTable("todos").ifExists().execute();
+	await db.schema.dropTable("tags").ifExists().execute();
+	await db.schema.dropTable("tag_groups").ifExists().execute();
 	await db.schema.dropTable("dates").ifExists().execute();
-	await db.schema.dropTable("project").ifExists().execute();
+	await db.schema.dropTable("projects").ifExists().execute();
 }
