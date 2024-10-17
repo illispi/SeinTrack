24a8daaf6604b9e8c78c1593e4c5bf17a6799dc3
illispi <44943503+illispi@users.noreply.github.com>
2024-10-17 06:03:31 +0300

    biome

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -3,7 +3,7 @@ import { A } from "@solidjs/router";
 export default function NotFound() {
 	return (
 		<main class="mx-auto p-4 text-center text-gray-700">
-			<h1 class="max-6-xs my-16 text-6xl font-thin uppercase text-sky-700">
+			<h1 class="max-6-xs my-16 font-thin text-6xl text-sky-700 uppercase">
 				Not Found
 			</h1>
 			<p class="mt-8">
