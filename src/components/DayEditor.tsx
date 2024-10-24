dd288d44f29817805da9e6d930772413574f7170
illispi <44943503+illispi@users.noreply.github.com>
2024-10-24 09:31:49 +0300

    delete time

--- src/components/DayEditor.tsx
+++ src/components/DayEditor.tsx
@@ -69,32 +69,33 @@ const DayEditor: Component<{
 
 	return (
 		<Suspense>
-			<div class="absolute left-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[expanded]:bg-accent data-[expanded]:text-muted-foreground">
-				<Button
-					onClick={() => {
-						if (props.selectedDate) {
-							console.log("daw");
-							changeHours.mutate({
-								date: props.selectedDate,
-								hours: 0,
-								projectId: props.projectId,
-							});
-						}
-					}}
+			<button
+				type="button"
+				onClick={() => {
+					if (props.selectedDate) {
+						console.log("daw");
+						changeHours.mutate({
+							date: props.selectedDate,
+							hours: 0,
+							projectId: props.projectId,
+						});
+					}
+				}}
+				class="absolute left-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[expanded]:bg-accent data-[expanded]:text-muted-foreground"
+			>
+				<svg
+					fill="currentColor"
+					stroke-width="0"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 1024 1024"
+					height="1em"
+					width="1em"
+					style="overflow: visible; color: currentcolor;"
+					class="size-6"
 				>
-					<svg
-						fill="currentColor"
-						stroke-width="0"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 1024 1024"
-						height="2em"
-						width="2em"
-						style="overflow: visible; color: currentcolor;"
-					>
-						<path d="M864 256H736v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zm-200 0H360v-72h304v72z"></path>
-					</svg>
-				</Button>
-			</div>
+					<path d="M864 256H736v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zm-200 0H360v-72h304v72z"></path>
+				</svg>
+			</button>
 			<div class="mx-auto flex w-full max-w-72 flex-col items-center justify-start gap-6">
 				<h4 class="text-3xl font-light">{props.selectedDate.toDateString()}</h4>
 				<h3 class="text-3xl">{`${hours.data?.hoursWorked || 0} h`}</h3>
