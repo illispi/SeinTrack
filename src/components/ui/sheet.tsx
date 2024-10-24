d3dba3c2c8987fc22586be86ce965bf45307cace
illispi <44943503+illispi@users.noreply.github.com>
2024-10-24 10:29:07 +0300

    sheet fix

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -112,7 +112,7 @@ const SheetContent = <T extends ValidComponent = "div">(
 						stroke-width="2"
 						stroke-linecap="round"
 						stroke-linejoin="round"
-						class="size-4"
+						class="size-6"
 					>
 						<path d="M18 6l-12 12" />
 						<path d="M6 6l12 12" />
