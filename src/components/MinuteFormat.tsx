cb7d5c0bcd376cdc84be7da5c0bcd9ae26967099
illispi <44943503+illispi@users.noreply.github.com>
2024-10-25 09:48:26 +0300

    svg change

--- src/components/MinuteFormat.tsx
+++ src/components/MinuteFormat.tsx
@@ -8,20 +8,18 @@ const MinuteFormat: Component<{ hours: number | null }> = (props) => {
 				when={props.hours}
 				fallback={
 					<svg
-						class="mt-1 size-4 lg:mt-2"
-						fill="currentColor"
-						stroke-width="0"
 						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 16 16"
-						// height="1.2em"
-						// width="1.2em"
-						style="overflow: visible; color: currentcolor;"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						// class="size-6"
+						class="mt-1 size-5 lg:mt-2"
 					>
-						<title>Cross</title>
-						<path
-							fill="currentColor"
-							d="M15.854 12.854 11 8l4.854-4.854a.503.503 0 0 0 0-.707L13.561.146a.499.499 0 0 0-.707 0L8 5 3.146.146a.5.5 0 0 0-.707 0L.146 2.439a.499.499 0 0 0 0 .707L5 8 .146 12.854a.5.5 0 0 0 0 .707l2.293 2.293a.499.499 0 0 0 .707 0L8 11l4.854 4.854a.5.5 0 0 0 .707 0l2.293-2.293a.499.499 0 0 0 0-.707z"
-						/>
+						<path d="M18 6l-12 12" />
+						<path d="M6 6l12 12" />
 					</svg>
 				}
 			>
