eb0d9192c95405b1022bc2c24d54a3c6944b8fcc
illispi <44943503+illispi@users.noreply.github.com>
2024-10-28 11:57:21 +0200

    eftgs

--- src/components/MinuteFormat.tsx
+++ src/components/MinuteFormat.tsx
@@ -3,7 +3,7 @@ import { hoursToFormat } from "~/utils/functionsAndVariables";
 
 const MinuteFormat: Component<{ hours: number | null }> = (props) => {
 	return (
-		<div class="flex h-full items-end justify-end">
+		<div class="flex h-8 items-center justify-center">
 			<Show
 				when={props.hours}
 				fallback={
@@ -16,14 +16,14 @@ const MinuteFormat: Component<{ hours: number | null }> = (props) => {
 						stroke-linecap="round"
 						stroke-linejoin="round"
 						// class="size-6"
-						class="mt-1 size-5 lg:mt-2"
+						class=" size-6"
 					>
 						<path d="M18 6l-12 12" />
 						<path d="M6 6l12 12" />
 					</svg>
 				}
 			>
-				<h3 class="font-semibold lg:text-xl">{`${props.hours ? hoursToFormat(props.hours).hours : hoursToFormat(0).hours}`}</h3>
+				<h3 class="text-center font-semibold lg:text-xl">{`${props.hours ? hoursToFormat(props.hours).hours : hoursToFormat(0).hours}`}</h3>
 				<Show
 					when={
 						props.hours
@@ -31,8 +31,8 @@ const MinuteFormat: Component<{ hours: number | null }> = (props) => {
 							: hoursToFormat(0).minutes
 					}
 				>
-					<span class="font-semibold lg:text-xl">:</span>
-					<h3 class="font-semibold lg:text-xl">{`${props.hours ? hoursToFormat(props.hours).minutes : hoursToFormat(0).minutes}`}</h3>
+					<span class="text-center font-semibold lg:text-xl">:</span>
+					<h3 class="text-center font-semibold lg:text-xl">{`${props.hours ? hoursToFormat(props.hours).minutes : hoursToFormat(0).minutes}`}</h3>
 				</Show>
 			</Show>
 		</div>
