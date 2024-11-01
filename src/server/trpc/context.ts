a223a08cd309e0ca4b1076ee539fd4ffbe1916d0
illispi <44943503+illispi@users.noreply.github.com>
2024-11-01 18:30:12 +0200

    changes

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -3,5 +3,6 @@ import type { createSolidAPIHandlerContext } from "@solid-mediakit/trpc/handler"
 import { db } from "~/server/database/db";
 
 export const createContext = async (opts: createSolidAPIHandlerContext) => {
+	console.log(process.env.DATABASE_URL);
 	return { db, req: opts.req, res: opts.res };
 };
